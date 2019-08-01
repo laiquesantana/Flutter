6bac0fd78c87bfd390de661322bc06adeb2d9ad7
laique santana <laiquesantana@hotmail.com>
2019-08-01 00:50:16 -0300

    gridview, box decoration etc

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Dog {
+  String nome;
+  String foto;
+
+  Dog(this.nome, this.foto);
+}
+
 class HelloListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,19 +19,45 @@ class HelloListView extends StatelessWidget {
   }
 
   _body() {
-    return ListView(
-      children: <Widget>[
-        _img('assets/images/capa2.jpeg'),
-        _img('assets/images/capa.jpeg')
-      ],
-    );
+    List<Dog> dogs = [
+      new Dog("Capa", "assets/images/capa.jpeg"),
+      new Dog("Capa2", "assets/images/capa2.jpeg"),
+      
+    ];
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: dogs.length,
+        itemBuilder: (context, index) {
+          Dog dog = dogs[index];
+
+          return Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              _img(dog.foto),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  margin: EdgeInsets.all(12),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(16)
+                  ),
+                  child: Text(
+                    dog.nome,
+                    style: TextStyle(color: Colors.blue, fontSize: 30),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
   }
 
-  _img(String path) {
+  Image _img(String path) {
     return Image.asset(
       path,
-      fit: BoxFit.contain,
+      fit: BoxFit.cover,
     );
   }
-
 }
